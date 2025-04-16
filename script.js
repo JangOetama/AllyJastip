1c0aa5a3e2711daebdc996642547b43df0181d1f
JangOetama <46255322+JangOetama@users.noreply.github.com>
2025-04-16 12:59:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -13,14 +13,11 @@ document.addEventListener('DOMContentLoaded', function() {
     };
 
     const renderTable = () => {
+        const jastipDiscount = parseFloat(document.getElementById('jastipDiscount').value) || 10;
         const min1 = parseInt(document.getElementById('min1').value) || 3;
         const min2 = parseInt(document.getElementById('min2').value) || 5;
         const min3 = parseInt(document.getElementById('min3').value) || 10;
-        const jastipDiscount = parseFloat(document.getElementById('jastipDiscount').value) || 10;
         const dpPercentage = parseFloat(document.getElementById('dpPercentage').value) || 50;
-        const min3Discount = parseFloat(document.getElementById('min3Discount').value) || 9;
-        const min5Discount = parseFloat(document.getElementById('min5Discount').value) || 8;
-        const min10Discount = parseFloat(document.getElementById('min10Discount').value) || 7;
 
         tableBody.innerHTML = '';
 
@@ -36,28 +33,31 @@ document.addEventListener('DOMContentLoaded', function() {
             const jastipPrice = originalPrice * (1 - (discountPercentage - jastipDiscount) / 100);
             const dpJastipPrice = jastipPrice * (1 - dpPercentage / 100);
 
-            const jastipPrice1 = originalPrice * (1 - (discountPercentage - min3Discount) / 100);
-            const jastipPrice2 = originalPrice * (1 - (discountPercentage - min5Discount) / 100);
-            const jastipPrice3 = originalPrice * (1 - (discountPercentage - min10Discount) / 100);
+            const jastipPrice1 = originalPrice * (1 - (discountPercentage - (jastipDiscount-1)) / 100);
+            const jastipPrice2 = originalPrice * (1 - (discountPercentage - (jastipDiscount-2)) / 100);
+            const jastipPrice3 = originalPrice * (1 - (discountPercentage - (jastipDiscount-3)) / 100);
 
             const formatPrice = (price) => new Intl.NumberFormat('id-ID').format(price);
 
             const description = `
                 ‼️ *Open PO Barang Lock N Lock ${product.name}*  \n
-                *Harga JKK :* 
-                ~Rp ${formatPrice(originalPrice)}~
-                Rp ${formatPrice(jastipPrice)}
-                *DP Rp ${formatPrice(dpJastipPrice)} /pcs*\n
-                *Skema Diskon :*
-                Min. ${min1} pcs : Rp ${formatPrice(jastipPrice1)} /pcs
-                Min. ${min2} pcs : Rp ${formatPrice(jastipPrice2)} /pcs
-                Min. ${min3} pcs : Rp ${formatPrice(jastipPrice3)} /pcs\n
-                *🌻 Deskripsi :*
-                ${product.description || 'Tidak ada deskripsi'}\n
-                \n
-                *🌱 Detail Order :*
-                * Close PO 17 April 25*
-                * Estimasi ready end Juni 25*
+            *Harga JKK :* 
+            ~Rp ${formatPrice(originalPrice)}~
+            Rp ${formatPrice(jastipPrice)}
+            *DP Rp ${formatPrice(dpJastipPrice)} /pcs*\n
+            *Skema Diskon :*
+            Min. ${min1} pcs : Rp ${formatPrice(jastipPrice1)} /pcs
+            Min. ${min2} pcs : Rp ${formatPrice(jastipPrice2)} /pcs
+            Min. ${min3} pcs : Rp ${formatPrice(jastipPrice3)} /pcs\n
+            *🌻 Deskripsi :*
+            ${product.description || 'Tidak ada deskripsi'}\n
+            \n
+            *🌱 Detail Order :*
+            * Close PO 17 April 25*
+            * Estimasi ready end Juni 25*\n\n
+            ====================
+            List : Nama + 4 Digit No WA
+            1. ...
             `;
             const row = document.createElement('tr');
             row.innerHTML = `
