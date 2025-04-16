1c9c153b2a262e3163c1e00a1fe1db638f061545
JangOetama <46255322+JangOetama@users.noreply.github.com>
2025-04-16 09:17:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const productData = [
-        { name: 'HLE313S2', originalPrice: 'Rp. 72,000', discountedPrice: 'Rp. 43,200', discountPercentage: '40%' },
-        { name: 'HLE314S3', originalPrice: 'Rp. 139,000', discountedPrice: 'Rp. 83,400', discountPercentage: '40%' },
-        { name: 'HPL817LB', originalPrice: 'Rp. 190,000', discountedPrice: 'Rp. 114,000', discountPercentage: '40%' }
-        // Tambahkan data produk lainnya di sini
-    ];
+    fetch('products.json')
+        .then(response => response.json())
+        .then(productData => {
+            const tableBody = document.querySelector('#productTable tbody');
 
-    const tableBody = document.querySelector('#productTable tbody');
-
-    productData.forEach(product => {
-        const row = document.createElement('tr');
-        row.innerHTML = `
-            <td>${product.name}</td>
-            <td>${product.originalPrice}</td>
-            <td>${product.discountedPrice}</td>
-            <td>${product.discountPercentage}</td>
-        `;
-        tableBody.appendChild(row);
-    });
+            productData.forEach(product => {
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                    <td>${product.name}</td>
+                    <td>${product.originalPrice}</td>
+                    <td>${product.discountedPrice}</td>
+                    <td>${product.discountPercentage}</td>
+                `;
+                tableBody.appendChild(row);
+            });
+        })
+        .catch(error => console.error('Error fetching product data:', error));
 });
